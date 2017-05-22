49e664f57688c26d968c20ddb3deff9ca049f59b
Rohit Surve <Rohit@symsoft.local>
2017-05-22 00:51:41 -0700

    Learning Handlebars

---
+++ LoginApp/routes/users.js
@@ -0,0 +1,15 @@
+var express= require('express');
+var router = express();
+
+// register
+router.get('/register', function(req, res){
+    res.render('register');
+});
+
+// login
+router.get('/login', function(req, res){
+    res.render('login');
+});
+
+
+module.exports = router;
